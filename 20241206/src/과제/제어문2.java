282866cbcf4aec64da898d4e78c1c248bc47bc04
zam_on <ky000612@naver.com>
2024-12-07 00:10:58 +0900

    prime number
    소수 구하기

--- 20241206/src/과제/제어문2.java
+++ 20241206/src/과제/제어문2.java
@@ -8,7 +8,7 @@ public class 제어문2 {
 		// 소수는 1과 자기자신을 제외한 수로 나누어 떨어지지 않으므로 1 제외
 		for (int i=2; i<=50; i++) {
 			int cnt = 0;
-			for (int j=2; i<i; j++) {
+			for (int j=2; j<i; j++) {
 				// 나누어 떨어지는 수가 있으면 cnt 1씩 증가 (1과 자기자신 제외)
 				if (i%j == 0) cnt++;
 			}
