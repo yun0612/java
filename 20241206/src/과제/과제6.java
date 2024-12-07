1b009d4762d250c3fe437e659571f538d20097a9
zam_on <ky000612@naver.com>
2024-12-07 19:45:13 +0900

    sum and average of array
    배열의 총점과 평균 구하기

---
+++ 20241206/src/과제/과제6.java
@@ -0,0 +1,20 @@
+package 과제;
+
+public class 과제6 {
+
+	public static void main(String[] args) {
+		/*배열의 총점과 평균 구하기*/
+		int[] a = {90, 80, 70, 60, 50};
+		int sum = 0;
+		double average;
+		
+		for (int i=0; i<a.length; i++) {
+			sum += a[i];
+		}
+		
+		average = sum / (double)a.length;
+		System.out.println("배열 a의 총점 = " + sum);
+		System.out.println("배열 a의 평균 = " + average);
+	}
+
+}
