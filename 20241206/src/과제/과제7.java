0dcd80908620e8cc5d983e501cc419a1e0e636c9
zam_on <ky000612@naver.com>
2024-12-07 19:50:39 +0900

    max and min of array
    배열의 최대값과 최소값 구하기

---
+++ 20241206/src/과제/과제7.java
@@ -0,0 +1,20 @@
+package 과제;
+
+public class 과제7 {
+
+	public static void main(String[] args) {
+		/* 배열의 최대값과 최소값 */
+		int[] a = {90, 80, 70, 60, 50, 10};
+		int max = a[0];
+		int min = a[0];
+		
+		for (int i=1; i<a.length; i++) {
+			if (max < a[i]) max = a[i];
+			if (min > a[i]) min = a[i];
+		}
+		
+		System.out.println("최대값=" + max);
+		System.out.println("최소값=" + min);
+	}
+
+}
