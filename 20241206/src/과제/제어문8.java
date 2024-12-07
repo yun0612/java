ccc86b8fd4fbeea09f89390de88cf79c2e2f3222
zam_on <ky000612@naver.com>
2024-12-07 20:03:11 +0900

    sort in a ascending order
    배열 오름차순 정렬하기

---
+++ 20241206/src/과제/제어문8.java
@@ -0,0 +1,24 @@
+package 과제;
+
+public class 제어문8 {
+
+	public static void main(String[] args) {
+		/* 배열을 오름차순으로 정렬 */
+		int[] a = {90, 80, 70, 60, 50, 10};
+		
+		for (int i=0; i<a.length-1; i++) {
+			for (int j=i+1; j<a.length; j++) {
+				if (a[i] > a[j]) {
+					int temp = a[i];
+					a[i] = a[j];
+					a[j] = temp;
+				}
+			}
+		}
+		
+		for (int i=0; i<a.length; i++) {
+			System.out.print(a[i] + " ");
+		}
+	}
+
+}
