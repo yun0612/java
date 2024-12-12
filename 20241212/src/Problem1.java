e76b586919ac2eb0c61e55d659b8f89cd91510fc
zam_on <ky000612@naver.com>
2024-12-12 12:38:31 +0900

    add method printing array
    배열 출력 메서드 추가

--- 20241212/src/Problem1.java
+++ 20241212/src/Problem1.java
@@ -1,9 +1,8 @@
 
 public class Problem1 {
 	// 20241206의 과제 패키지, 배열3.java를 메서드로 분리
-	// 결과 출력 코드 추가
 	public static void ascending(int[] arr) {
-		for (int i=0; i<arr.length; i++) {
+		for (int i=0; i<arr.length-1; i++) {
 			for (int j=i+1; j<arr.length; j++) {
 				if (arr[i] > arr[j]) {
 					int temp = arr[i];
@@ -11,6 +10,11 @@ public class Problem1 {
 					arr[j] = temp;
 				}
 			}
+		}
+	}
+	
+	public static void printArr(int[] arr) {
+		for (int i=0; i<arr.length; i++) {
 			System.out.printf("%d ", arr[i]);
 		}
 		System.out.println();
@@ -18,7 +22,12 @@ public class Problem1 {
 	
 	public static void main(String[] args) {
 		int[] a = {90, 80, 70, 60, 50, 10};
+		System.out.println("Before");
+		printArr(a);
+		
 		ascending(a);
+		System.out.println("After");
+		printArr(a);
 	}
 
 }
